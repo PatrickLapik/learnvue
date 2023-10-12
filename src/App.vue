f62f2d4c9cdb5be874907853272b49a2a3dbb94e
Patrick Lapimaa <patrick.lapimaa@tptlive.ee>
2023-10-12 11:31:53 +0300

    List example

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
 import { ref } from 'vue'
 const message = ref('Hello Vue')
+const items = ref(['sai', 'viin', 'viin'])
+function add(){
+  this.items.push(this.message)
+}
 </script>
 
 <template>
-  <button @click="message='Peenis'">Click me</button>
+  <button @click="items.add">Click me</button>
   <input type="text" v-model="message">
   <h1>{{ message.split('').reverse().join('') }}</h1>
+  <ul>
+    <li v-for="item in items">{{ item }}</li>
+  </ul>
 </template>
