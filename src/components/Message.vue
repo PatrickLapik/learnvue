0af13a6a4bfec6941c100a664254283f3ca72110
Patrick Lapimaa <patrick.lapimaa@tptlive.ee>
2023-12-07 11:38:55 +0200

    messages

---
+++ src/components/Message.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+defineProps(['open']);
+</script>
+
+<template>
+    <article class="message" :class="{'is-active': open}">
+        <div class="message-header">
+            <p>Hello World</p>
+            <button class="delete" aria-label="delete" @click="$emit('delete')" ></button>
+        </div>
+        <div class="message-content">
+            <slot></slot>
+        </div>
+    </article>
+</template>
---
+++ src/pages/Messages.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import { ref } from 'vue';
+    import Message from '../components/Message.vue'
+
+    let messageOpen = ref(false);
+
+</script>
+
+<template>
+    <button class="button is-primary" @click="messageOpen=true">Open Message</button>
+
+    <message :open="messageOpen" @close="messageOpen=false">
+        neeger.ee
+    </message>
+</template>
