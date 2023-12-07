f2a387a32eece257e3ef568799b15402b7836f2c
patricklapik <patrick.lapimaa@tptlive.ee>
2023-12-07 22:18:17 +0200

    Messages page

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,16 +1,49 @@
 <script setup>
-import { ref } from 'vue';
-defineProps(['open']);
+
+defineProps({
+    visible: {
+        type: Boolean,
+        required: true,
+    },
+    headerVisible: {
+        type: Boolean,
+    },
+    type: {
+        type: String,
+        default: 'info',
+        validator(value) {
+        return ['info', 'success', 'warning', 'danger'].includes(value)
+    }
+    },
+    size: {
+        type: String,
+        default: 'normal',
+        validator(value) {
+            return['small', 'normal', 'medium', 'large'].includes(value)
+        }
+    },
+    title: {
+        type: String,
+        default: 'Message',
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+    
+
+})
+
 </script>
 
 <template>
-    <article class="message" :class="{'is-active': open}">
-        <div class="message-header">
-            <p>Hello World</p>
-            <button class="delete" aria-label="delete" @click="$emit('delete')" ></button>
+    <div v-if="visible" class="message" :class="['is-' + type], ['is-' + size]">
+        <div v-if="headerVisible" class="message-header">
+            <p>{{ title }}</p>
+            <button class="delete" aria-label="delete" :class="['is-' + size]" @click="$emit('close')"></button>
         </div>
-        <div class="message-content">
-            <slot></slot>
+        <div class="message-body">
+            {{ message }}
         </div>
-    </article>
+    </div>
 </template>
--- src/pages/Messages.vue
+++ src/pages/Messages.vue
@@ -1,15 +1,21 @@
 <script setup>
-    import { ref } from 'vue';
-    import Message from '../components/Message.vue'
+import { ref } from 'vue';
+import Message from '../components/Message.vue';
 
-    let messageOpen = ref(false);
+let isMessageVisible = ref(false);
+let isMessageVisible2 = ref(false);
+
+
 
 </script>
 
 <template>
-    <button class="button is-primary" @click="messageOpen=true">Open Message</button>
+    <button class="button is-primary" @click="isMessageVisible = !isMessageVisible">Show Message</button>
+    <Message :visible="isMessageVisible" type="info" size="large" title="Message"
+        message="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore, reiciendis amet qui soluta nihil voluptatibus nulla sequi necessitatibus aliquam animi dolore repellat praesentium, similique maxime voluptates temporibus tempora cupiditate voluptas."/>
 
-    <message :open="messageOpen" @close="messageOpen=false">
-        neeger.ee
-    </message>
+    <button class="button is-primary" @click="isMessageVisible2 = true">Show Message 2</button>
+    <Message :visible="isMessageVisible2" headerVisible="false" type="danger" size="medium" title="Message"
+        message="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore, reiciendis amet qui soluta nihil voluptatibus nulla sequi necessitatibus aliquam animi dolore repellat praesentium, similique maxime voluptates temporibus tempora cupiditate voluptas."
+        @close="isMessageVisible2 = false" />
 </template>
