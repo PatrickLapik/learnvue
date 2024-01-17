95d7f23f932019bd1f67a9bad9873870a695bfdd
Patrick Lapimaa <patrick.lapimaa@tptlive.ee>
2024-01-17 11:01:26 +0200

    Chuck Norris jokeAPI

---
+++ src/pages/ChuckNorris.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="content">
+            <h2>{{ joke }}</h2>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+
+let joke = ref('')
+
+/* axios.get('https://api.chucknorris.io/jokes/random').then(respone => {
+    console.log(respone.data)
+    joke.value = respone.data.value
+}) */
+
+let respone = await axios.get('https://api.chucknorris.io/jokes/random')
+joke.value = respone.data.value
+
+</script>
