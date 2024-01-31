cc221d6cd304730e23ca602b66ce05ebc237f22a
Patrick Lapimaa <patrick.lapimaa@tptlive.ee>
2024-01-31 10:43:38 +0200

    Add infinite scroll

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -1,37 +1,17 @@
 <template>
     <div class="container">
         <div class="field has-addons">
-            <p class="control">
-                <button :disabled="!info.prev" class="button" @click="prev()">
-                    <span>Previous</span>
-                </button>
-            </p>
-
-            <p v-for="num in makePagination(page, info.pages)" :key="num" class="control">
-                <button 
-                    class="button" 
-                    :class="{'is-static': num==='...', 'is-primary': num===page}"
-                    @click="getCharacters(num)">
-                    <span>{{ num }}</span>
-                </button>
-            </p>
-
-            <p class="control">
-                <button :disabled="!info.next" class="button" @click="next()">
-                    <span>Next</span>
-                </button>
-            </p>
-        </div>
-        <div class="columns is-multiline">
-            <div v-for="char in chars" :key="char.id" class="column is-one-quarter">
-                <CharacterCard :character="char"></CharacterCard>
+            <div class="columns is-multiline">
+                <div v-for="char in chars" :key="char.id" class="column is-one-quarter">
+                    <CharacterCard :character="char"></CharacterCard>
+                </div>
             </div>
         </div>
     </div>
 </template>
 <script setup>
 import axios from 'axios';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import CharacterCard from '../components/CharacterCard.vue';
 
 let info = ref({});
@@ -45,35 +25,23 @@ async function getCharacters(pageNumber) {
             page: pageNumber
         }
     });
-    page.value=pageNumber;
-    console.log(response.data);
+    page.value = pageNumber;
     info.value = response.data.info;
-    chars.value = response.data.results;
+    chars.value.push(...response.data.results);
 }
 
-async function next(){
+async function next() {
     getCharacters(++page.value);
 }
 
-async function prev(){
-    getCharacters(--page.value);
-}
-
-function makePagination(current, total){
-    let pages = [];
-    for(let i=1;i<=3;i++){
-        pages[i] = i;
-    }
-    if(current>2 && current<total-1){
-        pages.push('...');
-        for(let i=current-1;i<=current+1;i++){
-            pages[i] = i;
+onMounted(() => {
+    document.addEventListener('scroll', () => {
+        if (window.scrollY + window.innerHeight > document.body.clientHeight - 200) {
+            next()
         }
-    }
-    pages.push('...');
-    for(let i=total-2; i<=total;i++){
-        pages[i] = i;
-    }
-    return pages.filter(value => value);
-}
+
+    })
+})
+
+
 </script>
