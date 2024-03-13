86c9a713b475f9daad1b2c34b8ea9299b628a3ae
PatrickLapik <patrick.lapimaa@tptlive.ee>
2024-03-13 11:18:26 +0200

    add wumo comics scraper

--- bin/terminal2.js
+++ bin/terminal2.js
@@ -1,26 +1,30 @@
-import axios from "axios";
-import * as cheerio from "cheerio";
-import fs from "fs";
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import fs from 'node:fs';
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
-if (!fs.existsSync("cache")) {
-  fs.mkdirSync("cache");
+if(!fs.existsSync('cache')){
+    fs.mkdirSync('cache');
 }
+let baseUrl = 'https://wumo.com';
+let link = '/wumo/2024/03/13';
 
-for (let i = 2905; i > 2895; i--) {
-  let cacheName = `cache/${i}.html`;
-  let data;
-  if (!fs.existsSync(`cache/${cacheName}`)) {
-    await sleep(1000);
-    let res = await axios.get(`https://xkcd.com/${i}/`);
-    data = res.data;
-    fs.writeFileSync(cacheName, data);
-  } else {
-    data = fs.readFileSync(cacheName);
-  }
-  const $ = cheerio.load(data);
-  let img = $("div#comic>img");
-  console.log(img.attr("src"));
-  console.log(img.attr("title"));
-  console.log(img.attr("alt"));
+for(let i = 0; i<10; i++){
+    let cacheName = link.replaceAll('/', '');
+    let cacheFile = `cache/${cacheName}.html`;
+    let data;
+    if(!fs.existsSync(cacheFile)){
+        await sleep(1000);
+        let res = await axios.get(baseUrl + link);
+        data = res.data;
+        fs.writeFileSync(cacheFile, data);
+    } else {
+        data = fs.readFileSync(cacheFile);
+    }
+    const $ = cheerio.load(data);
+    let img = $('div.box-content>a>img');
+    console.log(img.attr('src'));
+    console.log(img.attr('alt'));
+    let a = $('a.prev');
+    link = a.attr('href');
 }
